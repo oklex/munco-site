3752adea993c22f631477ecd64db4851f00207fc
oklex <alexkim.347@gmail.com>
2019-09-09 16:28:05 -0700

    added a referral btn for features

--- README.md
+++ README.md
@@ -3,8 +3,6 @@ MUNCO website
 This app interfaces with the munco wordpress API to create a dynamic UI for the organization's UX and 'business' goals.
 
 To-do:
-1. add blog post mini-nav
-    1. click to slide in, scroll element inside the window
 2. add UI for the Calendar
 3. add prototype endpoints for the Calendar
 4. add custom pages in React
--- src/containers/Blog/Blog.tsx
+++ src/containers/Blog/Blog.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import BlogList from '../../components/BlogList/BlogList'
+import BlogList from "../../components/BlogList/BlogList";
 import "./Blog.scss";
 const chilling: string = "/img/chilling.jpg";
 
 class Blog extends React.Component<{}, {}> {
-
   // componentDidMount = () => {
   //   window.scrollTo(0, 0);
   // };
-  
+
   render() {
     return (
       <div id="blog-home">
@@ -18,16 +17,32 @@ class Blog extends React.Component<{}, {}> {
             src={chilling}
             alt="student in suit playing accordion"
           ></img>
-          <div className='banner-overlay'>
-            <div className='banner-title'>
-            <h3>Features</h3>
+          <div className="banner-overlay">
+            <div className="banner-title">
+              
+              <h3>Features</h3>
             </div>
           </div>
         </div>
-        <div className='container'>
+        <div className="container">
           <h5>This is our Community</h5>
+          
+          <div className="referral-btn blueText">
+              <a href="https://forms.gle/rzHMmwdG7ay9LrCc6" target="_blank">
+                <p>refer someone you know &gt;</p>
+              </a>
+            </div>
         </div>
-        <BlogList/>
+        <BlogList />
+        <div className="container">
+          <div className="d-flex flex-row-reverse referral-btn">
+            <div className="referral-btn blueText">
+              <a href="https://forms.gle/rzHMmwdG7ay9LrCc6" target="_blank">
+                <p>refer someone you know &gt;</p>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
