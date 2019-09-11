5208fecab1116de39f596a93f818219083193b70
oklex <alexkim.347@gmail.com>
2019-09-11 16:33:06 -0700

    [WIP] Templating the application component

---
+++ src/components/Application/Application.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class Application extends React.Component<{},{}> {
+    render() {
+        return ( <div>
+            <p>Hello World</p>
+        </div>)
+    }
+}
+
+export default Application
+
+/* 
+TODO:
+1. click to open modal
+2. modal form sends an email
+3. formatting
+*/
