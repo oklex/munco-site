9fb55e7e1346cdd0f54f79d477fd84e19b0caa48
oklex <alexkim.347@gmail.com>
2019-09-16 14:21:47 -0700

    added a 'see all conferences' btn

---
+++ src/components/SingleEventLink/AllEventsLink.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import LinkStyle from "../../models/LinkStyle";
+import { Link } from "react-router-dom";
+
+interface IAllEventsLinkProps {
+  linkStyle: LinkStyle;
+}
+
+class AllEventsLink extends React.Component<IAllEventsLinkProps, {}> {
+  render() {
+    var styling: string = "";
+    if (this.props.linkStyle === LinkStyle.horizontal) styling = "col-lg-6";
+    if (this.props.linkStyle === LinkStyle.vertical) styling = "more-margin";
+    return (
+      <div className={styling}>
+        <Link to='/calendar'>
+          <div className="event-info AllEventsLink">
+            <h4 className='blueText'>See all conferences</h4>
+            <div>
+              <p>
+                a curated list for the lower mainland
+              </p>
+            </div>
+          </div>
+          </Link>
+      </div>
+    );
+  }
+}
+
+export default AllEventsLink
