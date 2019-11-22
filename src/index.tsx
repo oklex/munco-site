543c6b73f63e41e174634d7db9986656165e212b
oklex <alexkim.347@gmail.com>
2019-11-21 18:05:08 -0800

    connecting to firebase

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,21 @@ import App from './containers/App/App'
 import * as serviceWorker from './serviceWorker';
 import './styles/index.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import * as firebase from'firebase'
+
+require('dotenv').config()
+
+var firebaseConfig = {
+    apiKey: process.env.FIREBASE_APIKEY,
+    authDomain: process.env.FIREBASE_AUTHDOMAIN,
+    databaseURL:  process.env.FIREBASE_DATABASE_URL,
+    projectId:  process.env.FIREBASE_PROJET_ID,
+    storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId:  process.env.FIREBASE_APP_ID
+  };
+  
+  firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
