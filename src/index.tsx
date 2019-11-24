3c87696c6ed617e7036665526688e77caf5614c3
oklex <alexkim.347@gmail.com>
2019-11-24 14:21:04 -0800

    disconnected firebase

--- src/index.tsx
+++ src/index.tsx
@@ -6,19 +6,19 @@ import './styles/index.scss';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
 import * as firebase from'firebase'
 
-require('dotenv').config()
+// require('dotenv').config()
 
-var firebaseConfig = {
-    apiKey: process.env.FIREBASE_APIKEY,
-    authDomain: process.env.FIREBASE_AUTHDOMAIN,
-    databaseURL:  process.env.FIREBASE_DATABASE_URL,
-    projectId:  process.env.FIREBASE_PROJET_ID,
-    storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId:  process.env.FIREBASE_APP_ID
-  };
+// var firebaseConfig = {
+//     apiKey: process.env.FIREBASE_APIKEY,
+//     authDomain: process.env.FIREBASE_AUTHDOMAIN,
+//     databaseURL:  process.env.FIREBASE_DATABASE_URL,
+//     projectId:  process.env.FIREBASE_PROJET_ID,
+//     storageBucket:  process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId:  process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId:  process.env.FIREBASE_APP_ID
+//   };
   
-  firebase.initializeApp(firebaseConfig);
+//   firebase.initializeApp(firebaseConfig);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
