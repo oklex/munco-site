a2c2a328fb1243c2f05beefe06ad0c7f5bdea8c2
oklex <alexkim.347@gmail.com>
2020-01-05 15:10:07 -0800

    adding mock data

--- src/services/CalendarService/v2/CalendarData.ts
+++ src/services/CalendarService/v2/CalendarData.ts
@@ -12,8 +12,33 @@ export const AllCalendarData: ConferenceEvent[] = [
       start_date: new Date("February 14, 2020"),
       end_date: new Date("February 16, 2020"),
       running_since: new Date("January, 2002"),
-      tags: [],
-      events: []
+      tags: ["hotel conference", "running 10+ years"],
+      events: [
+        {
+          name: "Late Registration",
+          type: ApplicationType.Delegate,
+          start_date: new Date("January 1, 2020"),
+          end_date: new Date("February 10, 2020"),
+          dates_tentative: false,
+          cost: 215
+        },
+        {
+          name: "Secretariat Applications",
+          type: ApplicationType.Secretariat,
+          start_date: new Date("February 20, 2020"),
+          end_date: new Date("April 1, 2020"),
+          dates_tentative: true,
+          cost: null
+        },
+        {
+          name: "Staff hiring",
+          type: ApplicationType.Staff,
+          start_date: new Date(),
+          end_date: new Date(),
+          dates_tentative: true,
+          cost: null
+        }
+      ]
     },
 
   ];
