f37f9b2d0d4190f0ce9c50a474fdc90663eb0d6a
oklex <alexkim.347@gmail.com>
2020-01-13 20:25:19 -0800

    updated alignCenter on horizontal scroll

--- src/containers/Calendar/v2/Calendar.tsx
+++ src/containers/Calendar/v2/Calendar.tsx
@@ -45,6 +45,7 @@ export class CalendarV2 extends React.Component<{}, ICalendarState> {
           wheel={true}
           arrowLeft={ArrowLeft}
           arrowRight={ArrowRight}
+          alignCenter={false}
         />
       );
     }
