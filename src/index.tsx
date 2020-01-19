712c51de1c6c8e5cd08b9a41d77b8adb1e4e6e08
oklex <alexkim.347@gmail.com>
2020-01-18 21:34:31 -0800

    commented out Firebase config info for now

--- src/index.tsx
+++ src/index.tsx
@@ -9,18 +9,18 @@ import ClipboardJS from 'clipboard'
 
 require('dotenv').config()
 
-var firebaseConfig = {    
-    // apiKey:'AIzaSyAn4bf0wW3_JoqCLlldaUMKeXK2RiV1MeI',
-    // authDomain:'ci-prototype-6f628.firebaseapp.com',
-    // databaseURL:'https://ci-prototype-6f628.firebaseio.com',
-    // projectId: 'ci-prototype-6f628',
-    // storageBucket: 'ci-prototype-6f628.appspot.com',
-    // messagingSenderId: '209284948223',
-    // appId: '1:209284948223:web:9f451a8ee50dfd47a36cca'
-  };
+// var firebaseConfig = {    
+//     apiKey:'AIzaSyAn4bf0wW3_JoqCLlldaUMKeXK2RiV1MeI',
+//     authDomain:'ci-prototype-6f628.firebaseapp.com',
+//     databaseURL:'https://ci-prototype-6f628.firebaseio.com',
+//     projectId: 'ci-prototype-6f628',
+//     storageBucket: 'ci-prototype-6f628.appspot.com',
+//     messagingSenderId: '209284948223',
+//     appId: '1:209284948223:web:9f451a8ee50dfd47a36cca'
+//   };
   
   console.log('env test', process.env.TEST)
-  firebase.initializeApp(firebaseConfig);
+  // firebase.initializeApp(firebaseConfig);
   
   new ClipboardJS('.copy-btn');
 
