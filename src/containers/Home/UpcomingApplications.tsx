7521fe2cb0b42074a8002e157bee628be6ea0d5b
oklex <alexkim.347@gmail.com>
2020-06-01 01:54:54 -0700

    updated loading state update

--- src/containers/Home/UpcomingApplications.tsx
+++ src/containers/Home/UpcomingApplications.tsx
@@ -4,55 +4,57 @@ import CalendarService from "../../services/CalendarService/v2/CalendarService";
 import CalendarCard from "../../components/SingleEventLink/v2/CalendarCard";
 
 interface IUpcomingApplicationsState {
-  upcomingApplications: ICalendarResponse[];
-  loading: boolean;
+	upcomingApplications: ICalendarResponse[];
+	loading: boolean;
 }
 
 class UpcomingApplications extends React.Component<
-  {},
-  IUpcomingApplicationsState
+	{},
+	IUpcomingApplicationsState
 > {
-  state: IUpcomingApplicationsState = {
-    upcomingApplications: [],
-    loading: false,
-  };
+	state: IUpcomingApplicationsState = {
+		upcomingApplications: [],
+		loading: true,
+	};
 
-  componentDidMount = async () => {
-    let upcoming = await CalendarService.getUpcoming();
-    this.setState({
-      upcomingApplications: upcoming,
-    });
-  };
+	componentDidMount = async () => {
+		await CalendarService.getUpcoming().then((res) => {
+			this.setState({
+				upcomingApplications: res,
+				loading: false,
+			});
+		});
+	};
 
-  showUpcomingApps = () => {
-    if (this.state.loading) {
-      return <p> Loading . . .</p>;
-    } else if (this.state.upcomingApplications === []) {
-      return (
-        <div>
-          <h1>oops &#128552;</h1>
-          <p>
-            looks like there's nothing here <br />
-            check back later!
-          </p>
-        </div>
-      );
-    } else {
-      return this.state.upcomingApplications.map(
-        (conference: ICalendarResponse, index: number) => {
-          return (
-            <div key={index} className="col-md-6">
-              <CalendarCard CardDetails={conference} />
-            </div>
-          );
-        }
-      );
-    }
-  };
+	showUpcomingApps = () => {
+		if (this.state.loading) {
+			return <p> Loading . . .</p>;
+		} else if (this.state.upcomingApplications === []) {
+			return (
+				<div>
+					<h1>oops &#128552;</h1>
+					<p>
+						looks like there's nothing here <br />
+						check back later!
+					</p>
+				</div>
+			);
+		} else {
+			return this.state.upcomingApplications.map(
+				(conference: ICalendarResponse, index: number) => {
+					return (
+						<div key={index} className="col-md-6">
+							<CalendarCard CardDetails={conference} />
+						</div>
+					);
+				}
+			);
+		}
+	};
 
-  render() {
-    return <div className="row">{this.showUpcomingApps()}</div>;
-  }
+	render() {
+		return <div className="row">{this.showUpcomingApps()}</div>;
+	}
 }
 
 export default UpcomingApplications;
