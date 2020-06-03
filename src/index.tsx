4ac6a58f63cebb561c8635bae98423e1e41cdd6d
oklex <alexkim.347@gmail.com>
2020-06-03 16:28:32 -0700

    removed firebase comments

--- src/index.tsx
+++ src/index.tsx
@@ -1,29 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './containers/App/App'
-import * as serviceWorker from './serviceWorker';
-import './styles/index.scss';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./containers/App/App";
+import * as serviceWorker from "./serviceWorker";
+import "./styles/index.scss";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 // import * as firebase from'firebase'
-import ClipboardJS from 'clipboard'
+import ClipboardJS from "clipboard";
 
-require('dotenv').config()
+require("dotenv").config();
 
-// var firebaseConfig = {    
-//     apiKey:'AIzaSyAn4bf0wW3_JoqCLlldaUMKeXK2RiV1MeI',
-//     authDomain:'ci-prototype-6f628.firebaseapp.com',
-//     databaseURL:'https://ci-prototype-6f628.firebaseio.com',
-//     projectId: 'ci-prototype-6f628',
-//     storageBucket: 'ci-prototype-6f628.appspot.com',
-//     messagingSenderId: '209284948223',
-//     appId: '1:209284948223:web:9f451a8ee50dfd47a36cca'
-//   };
-  
-  // firebase.initializeApp(firebaseConfig);
-  
-  new ClipboardJS('.copy-btn');
+new ClipboardJS(".copy-btn");
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
